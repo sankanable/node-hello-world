c8e6cbd96321340cdd8e4d02af290fb87b591a3d
San Kanable <sankanable@example.com>
2025-06-25 09:20:24 +0000

    Fix duplicate express declaration

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const express = require('express')
+const express = require('express');
 const app = express();
 
 const port = 8080;
@@ -8,21 +8,7 @@ const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
   res.send('Hello World from IBM Cloud Essentials!');
-})
+});
 
 app.listen(port, host);
 console.log(`Running on http://${host}:${port}`);
-'use strict';
-
-const express = require('express')
-const app = express();
-
-const port = 8080;
-const host = '0.0.0.0';
-
-app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials!');
-})
-
-app.listen(port, host);
-/bin/bash: line 1: q: command not found
